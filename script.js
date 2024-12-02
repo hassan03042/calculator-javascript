9f403cdc00f50288f9b9856f82c2c21891dbfa4c
hassan03042 <muhammadhassan6948@gmail.com>
2024-12-03 00:41:48 +0500

    code updated

--- script.js
+++ script.js
@@ -6,12 +6,35 @@ function appendToDisplay(value) {
 
 function clearDisplay() {
     display.value = '';
+    animateDisplay();
 }
 
 function calculate() {
     try {
         display.value = eval(display.value);
+        animateDisplay();
     } catch (error) {
         display.value = 'Error';
+        animateDisplay();
     }
 }
+
+function animateDisplay() {
+    display.classList.add('bounce');
+    setTimeout(() => display.classList.remove('bounce'), 300);
+}
+
+// Keyboard support
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+
+    if (!isNaN(key) || ['+', '-', '*', '/'].includes(key)) {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        display.value = display.value.slice(0, -1);
+    } else if (key === 'c' || key === 'C') {
+        clearDisplay();
+    }
+});
